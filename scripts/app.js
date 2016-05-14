1c07089e4cc57ba77c594c38b19f943778151a7d
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-05-14 12:47:04 -0500

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+// empty array here tells angular to create an app called "todoListApp". notice that in helloWorld.js
+// you don't include the empty array because the app has already been created here. and this script list
+// is listed befor the helloWorld.js...so i think that's why it gets created first.
+angular.module("todoListApp", []);
