62cd59c01d1c14c343c92e7990de99fa55fbb9dd
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-05-14 22:23:02 -0500

    added a list of todos

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,19 @@
 // in ordor to make this a controller, I add the controller attribute
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope) {
-    $scope.helloWorld = function() {
-        console.log("this is the hello world controller function")
-    }
+    // $scope.helloWorld = function() {
+    //     console.log("this is the hello world controller function")
+    // }
+
+    // you can test that these variables are working by writing an angular expression
+    // anywhere inside the html.   {{todos}}
+    $scope.todos = [
+        {"name": "clean the house"},
+        {"name": "water the dog"},
+        {"name": "feed the lawn"},
+        {"name": "pay dem bills"}
+    ]
+
+
+
 });
