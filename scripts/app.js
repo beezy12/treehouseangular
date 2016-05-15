1b083b054d3ca74f86ba3058d91f0d9bda7b5f0a
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-05-15 14:41:07 -0500

    first services video done

--- scripts/app.js
+++ scripts/app.js
@@ -4,20 +4,49 @@
 
 // in ordor to make this a controller, I add the controller attribute
 angular.module("todoListApp", [])
-.controller('mainCtrl', function($scope) {
+.controller('mainCtrl', function($scope, dataService) {
+
+
+    $scope.helloConsole = dataService.helloConsole;
+
+    // when this controller is loaded, it fires the getTodos function below, which
+    // retrievs some data from the database and sticks it on the $scope.todos variable
+    dataService.getTodos(function(response) {
+        console.log(response.data);
+        $scope.todos = response.data;
+    });
+
     $scope.learningNgChange = function() {
         console.log("input changed!")
     }
 
     // you can test that these variables are working by writing an angular expression
     // anywhere inside the html.   {{todos}}
-    $scope.todos = [
-        {"name": "clean the house"},
-        {"name": "water the dog"},
-        {"name": "feed the lawn"},
-        {"name": "pay dem bills"}
-    ]
+
+    // we moved these to the mock folder. it is a mock of a database.
+
+    // $scope.todos = [
+    //     {"name": "clean the house"},
+    //     {"name": "water the dog"},
+    //     {"name": "feed the lawn"},
+    //     {"name": "pay dem bills"}
+    // ]
+
+})
+
+                            // inserting the $http here is called 'dependency injection'
+.service('dataService', function($http) {
+
+    this.helloConsole = function() {
+        console.log('this is the hello console service!')
+    };
 
 
+    this.getTodos = function(callback) {
+        $http.get('mock/todos.json')
+        .then(callback)
 
+    }
 });
+
+
