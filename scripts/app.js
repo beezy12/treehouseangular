8bbf7ae6ca7c113b1cbb81fd5b0753ce1aff7626
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-05-14 20:42:35 -0500

    understanding scope

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,11 @@
 // empty array here tells angular to create an app called "todoListApp". notice that in helloWorld.js
 // you don't include the empty array because the app has already been created here. and this script list
 // is listed befor the helloWorld.js...so i think that's why it gets created first.
-angular.module("todoListApp", []);
+
+// in ordor to make this a controller, I add the controller attribute
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope) {
+    $scope.helloWorld = function() {
+        console.log("this is the hello world controller function")
+    }
+});
